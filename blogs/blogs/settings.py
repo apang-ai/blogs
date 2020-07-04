3c3dde01f9dd79ebe372de9bc4b423dd9709a56d
apang <1305999617@qq.com>
2020-07-04 19:10:56 +0800

    '连接mysql数据库配置'

--- blogs/blogs/settings.py
+++ blogs/blogs/settings.py
@@ -77,10 +77,24 @@ WSGI_APPLICATION = 'blogs.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
+import pymysql
+pymysql.install_as_MySQLdb()
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'blogs',
+        'USER': 'root',
+        'PASSWORD': 'Myp199706',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
